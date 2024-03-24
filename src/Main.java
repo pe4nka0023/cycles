9da2d646a04cc477e58be7cfb7e0a195fb18cbbd
пккк <torr95157@gmail.com>
2024-03-25 00:31:57 +0300

    hello

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        //task 1
+        double sum = 0;
+        int mouth = 1;
+        while (sum < 2459000) {
+            sum += 15000;
+            System.out.println("Месяц " + mouth + ", сумма накоплений равна " + sum + " рублей");
+            mouth++;
+        }
+
+        //task 2
+
+    }
+}
