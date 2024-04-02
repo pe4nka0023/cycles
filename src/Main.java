ab5dc010fec66dd7abd192ba88611d475445bf04
пккк <torr95157@gmail.com>
2024-04-02 22:40:17 +0300

    helloo

--- src/Main.java
+++ src/Main.java
@@ -3,78 +3,75 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         //task 1
-        double sum = 0;
-        int mounth = 1;
-        while (sum < 2459000) {
-            sum += 15000;
-            System.out.println("Месяц " + mounth + ", сумма накоплений равна " + sum + " рублей");
-            mounth++;
-        }
-        //task 2
-        int n = 0;
-        while (n < 10) {
-            n++;
-            System.out.print(n + " ");
-        }
-        System.out.println();
-        for (; n >= 1; n--) {
-            System.out.print(n + " ");
+        int age = 17;
+        if (age < 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного " +
+                    "подождать");
+        } else {
+            System.out.println("Если возраст человека равен" + age + ", то он совершеннолетний");
         }
 
+        //task 2
+        int temp = 13;
+        if (temp > 5) {
+            System.out.println("На улице " + temp + " градусов, можно идти без шапки");
+        } else {
+            System.out.println("На улице" + temp + " градусов, нужно надеть шапку");
+        }
 
         //task 3
-        double population = 12000000;
-        int birthRate = 17;
-        int deathRate = 8;
-        for (int year = 1; year <= 10; year++) {
-            double birth = population / 1800 * birthRate;
-            double death = population / 1800 * deathRate;
-            population = population + birth - death;
-            System.out.println("Год " + year + " , численность населения составляет " + (long) population);
-            }
+        int speed = 67;
+        if (speed > 67) {
+            System.out.println("Если скорость" + speed + ", то придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость " + speed + " можно ездить спокойно");
+        }
 
         //task 4
-        sum = 15000;
-        mounth = 1;
-        while (sum < 12000000) {
-            sum = sum + sum * 7 / 100;
-            mounth++;
-            System.out.println("месяц = " + mounth + " сумма = " + (int) sum);
-            }
+        age = 19;
+        if (age >= 2 && age <= 6) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
+        } else if (age <= 17) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу");
+        } else if (age <= 24) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в универ");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить на работу");
+        }
 
         //task 5
-        sum = 15000;
-        mounth = 1;
-        while (sum < 12000000) {
-            sum = sum + sum * 7 / 100;
-            mounth++;
-            if (mounth % 6 == 0) {
-                System.out.println("месяц = " + mounth + " сумма = " + (int) sum);
-                }
-            }
-        //task 6
-        sum = 15000;
-        mounth = 1;
-        while (mounth < 9 * 12) {
-            sum = sum + sum * 7 / 100;
-            mounth++;
-            if (mounth % 6 == 0) {
-                System.out.println("месяц = " + mounth + " сумма = " + (int) sum);
-                }
-            }
-        //task 7
-        int friday = 3;
-        while (friday <= 31) {
-            System.out.println("Сегодня пятница, " + friday +"-е число. Необходимо подготовить отчет");
-            friday += 7;
+        age = 13;
+        if (age < 5) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему нельзя кататься на аттракционе");
+        } else if (age < 14) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе в " +
+                    "сопровождении взрослого");
+        } else {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе без " +
+                    "сопровождения взрослого");
         }
 
-        //task 8
-        int currentYear = LocalDate.now().getYear();
-        for (int year = currentYear - 200; year < currentYear + 100; year++) {
-            if (year % 79 == 0) {
-                System.out.println(year);
-            }
+        //task 6
+        int passengers = 74;
+        if (passengers < 60) {
+            System.out.println("есть сидячие места");
+        } else if (passengers <102) {
+            System.out.println("есть стоячие места");
+        } else {
+            System.out.println("мест нет");
         }
+
+        //task 7
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        if (one > two && one > three) {
+            System.out.println("one максимум");
+        } else if (two > three) {
+            System.out.println("two максимум");
+        } else {
+            System.out.println("three максимум");
+
         }
     }
+}
