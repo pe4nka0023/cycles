fb792924b3d13fac40f2828082a86d07e4a27d6b
пккк <torr95157@gmail.com>
2024-04-10 01:14:13 +0300

    arrays

--- src/Main.java
+++ src/Main.java
@@ -2,84 +2,76 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-
-
         //task 1
-        int os = 0;
-        if (os == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (os == 1) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Ваша  OS не поддерживается");
+        int[] nums = new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i + 1;
         }
+        double[] doubles = {1.57, 7.654, 9.986};
+        boolean[] bools = {false, false, true};
+
         //task 2
-        int year = 2013;
-        if (os == 0) {
-            if (year < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+            if (i < nums.length - 1) {
+                System.out.print(", ");
             }
-        } else if (os == 1) {
-            if (year < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            }
-        } else {
-            System.out.println("Ваш OS не поддерживается");
         }
+        System.out.println();
+        for (int i = 0; i < doubles.length; i++) {
+            System.out.print(doubles[i]);
+            if (i < doubles.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < bools.length; i++) {
+            System.out.print(bools[i]);
+            if (i < bools.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        System.out.println();
+
         //task 3
-        year = 2021;
-        if (year >= 1584 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
-            System.out.println(year + " год является високосным");
-        } else {
-            System.out.println( year + " год не является високосным");
+        for (int i = nums.length - 1; i >= 0; i--) {
+            System.out.print(nums[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
         }
+        System.out.println();
+        for (int i = doubles.length - 1; i >= 0; i--) {
+            System.out.print(doubles[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = bools.length - 1; i >= 0; i--) {
+            System.out.print(bools[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+
         //task 4
-        int days = 0;
-        int distance = 47;
-        if (distance < 20) {
-            days = 1;
-        } else if (distance < 60) {
-            days = 2;
-        } else if (distance < 100) {
-            days = 3;
-        } else {
-            days = -1;
+        System.out.println();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 != 0) {
+                nums[i]++;
+
+            }
+
         }
-        if (days >= 0) {
-            System.out.println("Потребуется дней: " + days + " дня");
-        } else {
-            System.out.println("Доставки нет");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+            if (i < nums.length - 1) {
+                System.out.print(", ");
+            }
+
         }
-        //task 5
-        int monthNumber = 11;
-        switch (monthNumber) {
-            case 12:
-            case 1:
-            case 2:
-                System.out.println("зима");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("весна");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("лето");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("осень");
-                break;
-            default:
-                System.out.println("такого месяца нет");
         }
 
     }
-}
