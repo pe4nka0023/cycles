6b38ce495bd16007d97038b9acf553715af016bd
пккк <torr95157@gmail.com>
2024-04-14 16:20:32 +0300

    hel

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,23 @@
 import java.time.LocalDate;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        double[] payments = {35000, 150000, 20000, 37000, 74000};
         //task 1
-        double sum = 0;
-        for (double payment : payments) {
-            sum += payment;
-        }
-        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+        var firstName = "Ivan";
+        var middleName = "Ivanov";
+        var lastName = "Ivanovich";
+        var fullName = middleName + " " + firstName + " " + lastName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
 
         //task 2
-        double min = Double.MAX_VALUE;
-        double max = Double.MIN_VALUE;
-        for (double payment : payments) {
-            if (payment < min) {
-                min = payment;
-            }
-            if (max < payment) {
-                max = payment;
-            }
-        }
-        System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей." +
-                "Максимальная сумма трат за неделю составила " + max + " рублей");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
 
         //task 3
-        double average = sum / payments.length;
-        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
-
-        //task 4
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i]);
-
-        }
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
         }
 
     }
