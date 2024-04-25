6a189e6a84461e574290f02ab35207ed870fb847
пккк <torr95157@gmail.com>
2024-04-25 23:31:53 +0300

    p

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,63 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        //task 1
-        var firstName = "Ivan";
-        var lastName = "Ivanov";
-        var middleName = "Ivanovich";
-        var fullName = middleName + " " + firstName + " " + lastName;
-        System.out.println("Ф. И. О. сотрудника — " + fullName);
+            //task 1
+            int year = 2012;
+            if (isLeapYear(year)) {
+                System.out.println(year + " год — високосный год");
+            } else {
+                System.out.println(year + " год — невисокосный год");
 
-        //task 2
-        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+            }
+            //task 2
+        printVersion(0,2024);
+            printVersion(1, 2020);
 
-
-        //task 3
-        fullName = "Иванов Семён Семёнович";
-        fullName = fullName.replace("ё", "е");
-        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
+            //task 3
+        int distance = 50;
+        int deliveryDays = calculateDeliveryDays(distance);
+        if (deliveryDays >= 0) {
+            System.out.println("Потребуется дней: " + deliveryDays);
+        } else {
+            System.out.println("Доставки нет");
         }
 
+        }
+    static boolean isLeapYear(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
+    static void printVersion(int os, int releaseYear){
+            int currentYear = LocalDate.now().getYear();
+            if (os == 0) {
+                if (releaseYear < currentYear) {
+                    System.out.println("Скачайте облегченную версию для ios");
+                } else {
+                    System.out.println("Скачайте версию для ios");
+                }
+            } else if (os == 1) {
+                if (releaseYear < currentYear) {
+                    System.out.println("Скачайте облегченную версию для android");
+                } else {
+                    System.out.println("Скачайте версию для android");
+                }
+            } else {
+                System.out.println("Устройство не поддерживается");
+
+            }
+        }
+
+        static int  calculateDeliveryDays(int distance) {
+            if (distance <= 20) {
+                return 1;
+            }else if (distance <= 60) {
+                return 2;
+            } else if (distance <= 100) {
+                return 3;
+            } else {
+                return -1;
+            }
+        }
+
+        }
+
+    
